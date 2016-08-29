f16768dfda29960564069b89044c6fb5a30a6c0d
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-29 13:22:09 +0200

    Hours and minutes can be parsed as well.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,15 @@
 import { assertThat, equalTo } from 'hamjest';
 
 const parseIso = (isoString) => {
-  const [year, month, day] = isoString.split('-');
+  const [dateComponent, timeComponent = ''] = isoString.split('T');
+  const [year, month, day] = dateComponent.split('-');
+  const [hour, minute] = timeComponent.split(':');
   return {
     year: parseInt(year),
     month: month ? parseInt(month) : 1,
     day: day ? parseInt(day) : 1,
+    hour: parseInt(hour),
+    minute: parseInt(minute),
   };
 };
 
@@ -41,5 +45,24 @@ describe('parseIso', () => {
 
     it('day 1', () => assertThat(
       parseIso('2000-01-01').day, equalTo(1)));
+  });
+
+  describe('`2000-01-01` responds', () => {
+    it('year 2000', () => assertThat(
+      parseIso('2000-01-01').year, equalTo(2000)));
+
+    it('month 1', () => assertThat(
+      parseIso('2000-01-01').month, equalTo(1)));
+
+    it('day 1', () => assertThat(
+      parseIso('2000-01-01').day, equalTo(1)));
+  });
+
+  describe('`2000-01-01T10:20` responds', () => {
+    it('`hour is 10`', () => assertThat(
+      parseIso('2000-01-01T10:20').hour, equalTo(10)));
+
+    it('`minute is 20`', () => assertThat(
+      parseIso('2000-01-01T10:20').minute, equalTo(20)));
   });
 });
