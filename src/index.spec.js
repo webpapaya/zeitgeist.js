099d757aeb8c90c88a5e54cc4a3d144a2b57e8c2
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-29 15:11:50 +0200

    Added daysInMonth functionality.

--- src/index.spec.js
+++ src/index.spec.js
@@ -125,7 +125,8 @@ describe('parseIso', () => {
   });
 });
 
-const isLeapYear = (year) => {
+const isLeapYear = (isoString) => {
+  const year = parseIso(isoString).year;
   const dividableBy4 = year % 4 === 0;
   const dividableBy100 = year % 100 === 0;
   const dividableBy400 = year % 400 === 0;
@@ -154,11 +155,96 @@ describe('isLeapYear', () => {
 
   LEAP_YEARS.forEach((year) => {
     it(`${year} is a leap year`, () => assertThat(
-      isLeapYear(year), equalTo(true)));
+      isLeapYear(`${year}`), equalTo(true)));
   });
 
   REGULAR_YEARS.forEach((year) => {
     it(`${year} is NOT a leap year`, () => assertThat(
-      isLeapYear(year), equalTo(false)));
+      isLeapYear(`${year}`), equalTo(false)));
   });
 });
+
+const DAYS_IN_MONTHS = [31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31];
+const daysInMonth = (isoString, month) => {
+  if(month === 2 && isLeapYear(isoString) ) { return 29; }
+  return DAYS_IN_MONTHS[month - 1];
+};
+
+describe('daysInMonth', () => {
+  describe('in a leap year', () => {
+    it('january has 31 days', () => assertThat(
+      daysInMonth('2016', 1), equalTo(31)));
+
+    it('february has 29 days', () => assertThat(
+      daysInMonth('2016', 2), equalTo(29)));
+
+    it('march has 31 days', () => assertThat(
+      daysInMonth('2016', 3), equalTo(31)));
+
+    it('april has 30 days', () => assertThat(
+      daysInMonth('2016', 4), equalTo(30)));
+
+    it('may has 31 days', () => assertThat(
+      daysInMonth('2016', 5), equalTo(31)));
+
+    it('june has 30 days', () => assertThat(
+      daysInMonth('2016', 6), equalTo(30)));
+
+    it('july has 30 days', () => assertThat(
+      daysInMonth('2016', 7), equalTo(30)));
+
+    it('august has 31 days', () => assertThat(
+      daysInMonth('2016', 8), equalTo(31)));
+
+    it('september has 30 days', () => assertThat(
+      daysInMonth('2016', 9), equalTo(30)));
+
+    it('october has 31 days', () => assertThat(
+      daysInMonth('2016', 10), equalTo(31)));
+
+    it('november has 30 days', () => assertThat(
+      daysInMonth('2016', 11), equalTo(30)));
+
+    it('december has 31 days', () => assertThat(
+      daysInMonth('2016', 12), equalTo(31)));
+  });
+
+  describe('in a regular year', () => {
+    it('january has 31 days', () => assertThat(
+      daysInMonth('2017', 1), equalTo(31)));
+
+    it('february has 28 days', () => assertThat(
+      daysInMonth('2017', 2), equalTo(28)));
+
+    it('march has 31 days', () => assertThat(
+      daysInMonth('2017', 3), equalTo(31)));
+
+    it('april has 30 days', () => assertThat(
+      daysInMonth('2017', 4), equalTo(30)));
+
+    it('may has 31 days', () => assertThat(
+      daysInMonth('2017', 5), equalTo(31)));
+
+    it('june has 30 days', () => assertThat(
+      daysInMonth('2017', 6), equalTo(30)));
+
+    it('july has 30 days', () => assertThat(
+      daysInMonth('2017', 7), equalTo(30)));
+
+    it('august has 31 days', () => assertThat(
+      daysInMonth('2017', 8), equalTo(31)));
+
+    it('september has 30 days', () => assertThat(
+      daysInMonth('2017', 9), equalTo(30)));
+
+    it('october has 31 days', () => assertThat(
+      daysInMonth('2017', 10), equalTo(31)));
+
+    it('november has 30 days', () => assertThat(
+      daysInMonth('2017', 11), equalTo(30)));
+
+    it('december has 31 days', () => assertThat(
+      daysInMonth('2017', 12), equalTo(31)));
+  });
+});
+
