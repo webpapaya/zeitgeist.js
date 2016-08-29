6fe73341aeee70315be54b99cdc229106bdf322d
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-29 13:11:35 +0200

    parseIso responds correct day.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,11 @@
 import { assertThat, equalTo } from 'hamjest';
 
 const parseIso = (isoString) => {
-  const [year, month] = isoString.split('-');
+  const [year, month, day] = isoString.split('-');
   return {
     year: parseInt(year),
     month: parseInt(month),
+    day: parseInt(day),
   };
 };
 
@@ -14,4 +15,7 @@ describe('parseIso', () => {
 
   it('`2000-01` responds month 1', () => assertThat(
     parseIso('2000-01').month, equalTo(1)));
+
+  it('`2000-01-01` responds day 1', () => assertThat(
+    parseIso('2000-01-01').day, equalTo(1)));
 });
