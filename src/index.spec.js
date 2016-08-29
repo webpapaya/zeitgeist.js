f6c9de0f159c3abe8561b5b161954fcdb11d6e14
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-29 15:15:34 +0200

    Extracted into own file.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,41 +1,7 @@
 import { assertThat, equalTo } from 'hamjest';
+import { parseIso, daysInMonth, isLeapYear } from './index';
 
-const containsChar = (isoString, s) => isoString.indexOf(s) !== -1;
 
-const DATE_UNIT_SEPARATOR = '-';
-const TIME_UNIT_SEPARATOR = ':';
-
-const TIME_COMPONENT_SEPARATOR_1 = 'T';
-const TIME_COMPONENT_SEPARATOR_2 = ' ';
-
-const parseDateUnit = (value) => value ? parseInt(value, 10) : 1;
-const parseTimeUnit = (value) => value ? parseFloat(value) : 0;
-
-const findTimeSeperator = (isoString) => {
-  if(containsChar(isoString, TIME_COMPONENT_SEPARATOR_1)) { return TIME_COMPONENT_SEPARATOR_1; }
-  return TIME_COMPONENT_SEPARATOR_2;
-};
-
-const separateDateAndTimeComponents = (isoString) => {
-  const timeSeparator = findTimeSeperator(isoString);
-  const [dateComponent, timeComponent = ''] = isoString.split(timeSeparator);
-  return { dateComponent, timeComponent };
-};
-
-const parseIso = (isoString) => {
-  const { dateComponent, timeComponent } = separateDateAndTimeComponents(isoString);
-  const [year, month, day] = dateComponent.split(DATE_UNIT_SEPARATOR);
-  const [hour, minute, second] = timeComponent.split(TIME_UNIT_SEPARATOR);
-
-  return {
-    year: parseInt(year),
-    month: parseDateUnit(month),
-    day: parseDateUnit(day),
-    hour: parseTimeUnit(hour),
-    minute: parseTimeUnit(minute),
-    second: parseTimeUnit(second),
-  };
-};
 
 describe('parseIso', () => {
   describe('`2000` responds', () => {
@@ -125,15 +91,6 @@ describe('parseIso', () => {
   });
 });
 
-const isLeapYear = (isoString) => {
-  const year = parseIso(isoString).year;
-  const dividableBy4 = year % 4 === 0;
-  const dividableBy100 = year % 100 === 0;
-  const dividableBy400 = year % 400 === 0;
-
-  return (dividableBy4 && !dividableBy100) || dividableBy400;
-};
-
 describe('isLeapYear', () => {
   const LEAP_YEARS = '2000 2016'.split(' ');
   const REGULAR_YEARS = '1582 1700 1800 2017'.split(' ');
@@ -149,11 +106,6 @@ describe('isLeapYear', () => {
   });
 });
 
-const DAYS_IN_MONTHS = [31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31];
-const daysInMonth = (isoString, month) => {
-  if(month === 2 && isLeapYear(isoString) ) { return 29; }
-  return DAYS_IN_MONTHS[month - 1];
-};
 
 describe('daysInMonth', () => {
   describe('in a leap year', () => {
