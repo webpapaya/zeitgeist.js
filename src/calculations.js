bcb1cd98af20ea5bdec51f4caef10205b8cf6271
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-05 23:13:26 +0200

    Added substract days method.

--- src/calculations.js
+++ src/calculations.js
@@ -1,4 +1,4 @@
-import { DECEMBER } from './constants';
+import { DECEMBER, JANUARY } from './constants';
 import { toFragments, toIso, daysInMonth, daysInYear, isLastDayOfMonth } from './index';
 
 
@@ -24,6 +24,7 @@ const jumpToNextDay = (isoString) => {
 
 export const addDays = (isoString, days) => {
   if(days === 0) { return isoString; }
+  if(days < 0) { return subtractDays(isoString, days * -1); }
 
   if(isLastDayOfYear(isoString)) {
     return addDays(jumpToNextYear(isoString), days - 1);
@@ -34,4 +35,50 @@ export const addDays = (isoString, days) => {
   }
 
   return addDays(jumpToNextDay(isoString), days - 1);
+};
+
+const isFirstDayOfYear = (isoString) => {
+  const { month, day } = toFragments(isoString);
+  return month === JANUARY && day === 1;
+};
+
+const isFirstDayOfMonth = (isoString) => {
+  const { day } = toFragments(isoString);
+  return day === 1;
+};
+
+const jumpToPreviousDay = (isoString) => {
+  const fragments = toFragments(isoString);
+  return toIso({ ...fragments, day: fragments.day - 1 });
+};
+
+const jumpToPreviousYear = (isoString) => {
+  const fragments = toFragments(isoString);
+  return toIso({ ...fragments, year: fragments.year - 1, month: 12, day: 31 });
+};
+
+const jumpToPreviousMonth = (isoString) => {
+  const fragments = toFragments(isoString);
+  const previousMonth = toIso({
+    year: fragments.year,
+    month: fragments.month === JANUARY ? DECEMBER : fragments.month - 1
+  });
+  const day = daysInMonth(previousMonth);
+
+  return toIso({ ...fragments, month: fragments.month - 1, day: day });
+};
+
+export const subtractDays = (isoString, days) => {
+  if(days === 0) { return isoString; }
+  if(days < 0) { return addDays(isoString, days * -1); }
+
+  if(isFirstDayOfYear(isoString)) {
+    return subtractDays(jumpToPreviousYear(isoString), days - 1);
+  }
+
+  if(isFirstDayOfMonth(isoString)) {
+    return subtractDays(jumpToPreviousMonth(isoString), days - 1);
+  }
+
+  return subtractDays(jumpToPreviousDay(isoString), days - 1);
 };
--- src/calculations.spec.js
+++ src/calculations.spec.js
@@ -1,7 +1,7 @@
 import { assertThat, equalTo, not, throws } from 'hamjest';
-import { addDays } from './calculations';
+import { addDays, subtractDays } from './calculations';
 
-describe.only('addDays', () => {
+describe('addDays', () => {
   it('adding 1 day to 2000-01-01 results in 2000-01-02', () => assertThat(
     addDays('2000-01-01', 1), equalTo('2000-01-02')));
 
@@ -14,10 +14,35 @@ describe.only('addDays', () => {
   it('adding 1 day to 2000-01-31 results in 2000-02-01', () => assertThat(
     addDays('2000-01-31', 1), equalTo('2000-02-01')));
 
-  it('adding 2 days to 2000-01-31 results in 2000-02-01', () => assertThat(
+  it('adding 2 days to 2000-01-31 results in 2000-02-02', () => assertThat(
     addDays('2000-01-31', 2), equalTo('2000-02-02')));
 
   it('adding 365 days to 2000-01-01 results in 2001-01-01', () => assertThat(
     addDays('2000-01-01', 365), equalTo('2001-01-01')));
+
+  it('adding -1 day to 2001-01-01 results in 2000-12-31', () => assertThat(
+    addDays('2001-01-01', -1), equalTo('2000-12-31')));
 });
 
+describe('subtractDays', () => {
+  it('subtract 1 day to 2000-01-02 results in 2000-01-01', () => assertThat(
+    subtractDays('2000-01-02', 1), equalTo('2000-01-01')));
+
+  it('subtract 31 days to 2000-02-01 results in 2000-01-01', () => assertThat(
+    subtractDays('2000-02-01', 31), equalTo('2000-01-01')));
+
+  it('subtract 31 + 29 days to 2000-03-01 results in 2000-01-01', () => assertThat(
+    subtractDays('2000-03-01', 60), equalTo('2000-01-01')));
+
+  it('subtract 1 day to 2001-01-01 results in 2000-12-31', () => assertThat(
+    subtractDays('2001-01-01', 1), equalTo('2000-12-31')));
+
+  it('subtract 2 days to 2000-02-02 results in 2000-01-31', () => assertThat(
+    subtractDays('2000-02-02', 2), equalTo('2000-01-31')));
+
+  it('subtract 365 days to 2001-01-01 results in 2000-01-01', () => assertThat(
+    subtractDays('2001-01-01', 365), equalTo('2000-01-01')));
+
+  it('subtract -1 day to 2000-12-31 results in 2001-01-01', () => assertThat(
+    subtractDays('2000-12-31', -1), equalTo('2001-01-01')));
+});
