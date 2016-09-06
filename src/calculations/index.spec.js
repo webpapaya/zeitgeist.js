8b1984d282b6c80def896ebf72a2755cf706c039
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-06 08:30:32 +0200

    Fixed typos in test names.

--- src/calculations/index.spec.js
+++ src/calculations/index.spec.js
@@ -24,25 +24,26 @@ describe('addDays', () => {
     addDays('2001-01-01', -1), equalTo('2000-12-31')));
 });
 
+
 describe('subtractDays', () => {
-  it('subtract 1 day to 2000-01-02 results in 2000-01-01', () => assertThat(
+  it('subtract 1 day from 2000-01-02 results in 2000-01-01', () => assertThat(
     subtractDays('2000-01-02', 1), equalTo('2000-01-01')));
 
-  it('subtract 31 days to 2000-02-01 results in 2000-01-01', () => assertThat(
+  it('subtract 31 days from 2000-02-01 results in 2000-01-01', () => assertThat(
     subtractDays('2000-02-01', 31), equalTo('2000-01-01')));
 
-  it('subtract 31 + 29 days to 2000-03-01 results in 2000-01-01', () => assertThat(
+  it('subtract 31 + 29 days from 2000-03-01 results in 2000-01-01', () => assertThat(
     subtractDays('2000-03-01', 60), equalTo('2000-01-01')));
 
-  it('subtract 1 day to 2001-01-01 results in 2000-12-31', () => assertThat(
+  it('subtract 1 day from 2001-01-01 results in 2000-12-31', () => assertThat(
     subtractDays('2001-01-01', 1), equalTo('2000-12-31')));
 
-  it('subtract 2 days to 2000-02-02 results in 2000-01-31', () => assertThat(
+  it('subtract 2 days from 2000-02-02 results in 2000-01-31', () => assertThat(
     subtractDays('2000-02-02', 2), equalTo('2000-01-31')));
 
-  it('subtract 365 days to 2001-01-01 results in 2000-01-01', () => assertThat(
+  it('subtract 365 days from 2001-01-01 results in 2000-01-01', () => assertThat(
     subtractDays('2001-01-01', 365), equalTo('2000-01-01')));
 
-  it('subtract -1 day to 2000-12-31 results in 2001-01-01', () => assertThat(
+  it('subtract -1 day from 2000-12-31 results in 2001-01-01', () => assertThat(
     subtractDays('2000-12-31', -1), equalTo('2001-01-01')));
 });
