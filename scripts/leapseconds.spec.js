49a823e381c07dd60db063a5907262221f4bb336
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-06 12:57:44 +0200

    Extracted readLeapSeconds into own file.

--- scripts/leapseconds.spec.js
+++ scripts/leapseconds.spec.js
@@ -1,36 +1,7 @@
 import { readFileSync } from 'fs';
-
-const TZ_MONTHS = {
-  Jan: '01',
-  Feb: '02',
-  Mar: '03',
-  Apr: '04',
-  May: '05',
-  Jun: '06',
-  Jul: '07',
-  Aug: '08',
-  Sep: '09',
-  Oct: '10',
-  Nov: '11',
-  Dec: '12',
-};
-
-const readLeapSeconds = (fileContents) => {
-  return fileContents.split('\n')
-    .filter((line) => line.startsWith('Leap'))
-    .map((line) => line.split('\t'))
-    .reduce((previous, [ type, year, month, day, time, correction ]) => {
-      return {
-        ...previous,
-        [`${year}-${TZ_MONTHS[month]}-${day}`]: {
-          correction: correction === '+' ? 1 : -1,
-          time: time
-        }
-      };
-    }, {});
-};
-
 import { assertThat, equalTo } from 'hamjest';
+import { readLeapSeconds } from './leapseconds';
+
 describe('readLeapSeconds', () => {
   it('removes all comments', () => {
     const tzLeapSeconds = readFileSync('./scripts/_test-fixtures/leapseconds', 'utf8');
