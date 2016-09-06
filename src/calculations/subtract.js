d0501cfcdef38c2acc2c086db81ee1db966b43d1
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-06 10:51:15 +0200

    Remove toFragments fns.

--- src/calculations/subtract.js
+++ src/calculations/subtract.js
@@ -2,28 +2,16 @@ import { DECEMBER, JANUARY } from '../constants';
 import { addDays } from './add'
 import { toFragments, toIso, daysInMonth } from '../index';
 
-const isFirstDayOfYear = (isoString) => {
-  const { month, day } = toFragments(isoString);
-  return month === JANUARY && day === 1;
-};
+const isFirstDayOfYear = ({ month, day }) => month === JANUARY && day === 1;
+const isFirstDayOfMonth = ({ day }) => day === 1;
 
-const isFirstDayOfMonth = (isoString) => {
-  const { day } = toFragments(isoString);
-  return day === 1;
-};
+const jumpToPreviousDay = (fragments) =>
+  toIso({ ...fragments, day: fragments.day - 1 });
 
-const jumpToPreviousDay = (isoString) => {
-  const fragments = toFragments(isoString);
-  return toIso({ ...fragments, day: fragments.day - 1 });
-};
+const jumpToPreviousYear = (fragments) =>
+  toIso({ ...fragments, year: fragments.year - 1, month: 12, day: 31 });
 
-const jumpToPreviousYear = (isoString) => {
-  const fragments = toFragments(isoString);
-  return toIso({ ...fragments, year: fragments.year - 1, month: 12, day: 31 });
-};
-
-const jumpToPreviousMonth = (isoString) => {
-  const fragments = toFragments(isoString);
+const jumpToPreviousMonth = (fragments) => {
   const previousMonth = toIso({
     year: fragments.year,
     month: fragments.month === JANUARY ? DECEMBER : fragments.month - 1
@@ -37,15 +25,17 @@ export const subtractDays = (isoString, days) => {
   if(days === 0) { return isoString; }
   if(days < 0) { return addDays(isoString, days * -1); }
 
-  if(isFirstDayOfYear(isoString)) {
-    return subtractDays(jumpToPreviousYear(isoString), days - 1);
+  const fragments = toFragments(isoString);
+
+  if(isFirstDayOfYear(fragments)) {
+    return subtractDays(jumpToPreviousYear(fragments), days - 1);
   }
 
-  if(isFirstDayOfMonth(isoString)) {
-    return subtractDays(jumpToPreviousMonth(isoString), days - 1);
+  if(isFirstDayOfMonth(fragments)) {
+    return subtractDays(jumpToPreviousMonth(fragments), days - 1);
   }
 
-  return subtractDays(jumpToPreviousDay(isoString), days - 1);
+  return subtractDays(jumpToPreviousDay(fragments), days - 1);
 };
 
 export const subtractYears = (isoString, years) => {
