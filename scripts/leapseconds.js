23b94c99de0fd782dee1de38a9f6d12bda1a7b76
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-06 12:59:47 +0200

    Extracted constants.

--- scripts/leapseconds.js
+++ scripts/leapseconds.js
@@ -1,3 +1,7 @@
+const NEWLINE = '\n';
+const TAB = '\t';
+
+const TZ_LEAP_IDENTIFIER = 'Leap';
 const TZ_MONTHS = {
   Jan: '01',
   Feb: '02',
@@ -14,9 +18,9 @@ const TZ_MONTHS = {
 };
 
 export const readLeapSeconds = (fileContents) => {
-  return fileContents.split('\n')
-    .filter((line) => line.startsWith('Leap'))
-    .map((line) => line.split('\t'))
+  return fileContents.split(NEWLINE)
+    .filter((line) => line.startsWith(TZ_LEAP_IDENTIFIER))
+    .map((line) => line.split(TAB))
     .reduce((previous, [ type, year, month, day, time, correction ]) => {
       return {
         ...previous,
