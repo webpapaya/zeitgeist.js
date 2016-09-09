fb016d2095d5f0497e89bba2f15967a469752382
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-09 11:46:24 +0200

    Added guard to getCallStackSize if tail call optimisation is enabled.

--- src/test-helper.js
+++ src/test-helper.js
@@ -2,6 +2,7 @@ export const getCallStackSize = () => {
   let i = 0;
   try {
     const stackSizeTester = () => {
+      if(i >= 10e6) { return 10e6; }
       i++; stackSizeTester();
     };
     stackSizeTester();
