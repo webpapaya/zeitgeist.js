52d66400427fd26d9a6c884d687a577030b5f997
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-10 17:31:47 +0200

    Extracted into en.js

--- src/locales/default-locale.js
+++ src/locales/default-locale.js
@@ -1,7 +1,1 @@
-export const locale = {
-  weekdaysLong: 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split(' '),
-  weekdaysShort: 'Mon Tue Wed Thu Fri Sat Sun'.split(' '),
-
-  monthsLong: 'January February March April May June July August September October November December'.split(' '),
-  monthsShort: 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
-};
+export { locale } from './en';
---
+++ src/locales/en.js
@@ -0,0 +1,6 @@
+export const locale = {
+  weekdaysLong: 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split(' '),
+  weekdaysShort: 'Mon Tue Wed Thu Fri Sat Sun'.split(' '),
+  monthsLong: 'January February March April May June July August September October November December'.split(' '),
+  monthsShort: 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+};
