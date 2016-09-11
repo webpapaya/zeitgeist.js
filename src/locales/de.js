926bd0de75c9c9f32d7fd0d9c7d697bad50fb3a8
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-11 19:10:21 +0200

    Added de locale.

---
+++ src/locales/de.js
@@ -0,0 +1,6 @@
+export const locale = {
+  monthsLong: 'Januar_Februar_März_April_Mai_Juni_Juli_August_September_Oktober_November_Dezember'.split('_'),
+  monthsShort: 'Jan._Febr._Mrz._Apr._Mai_Jun._Jul._Aug._Sept._Okt._Nov._Dez.'.split('_'),
+  weekdaysLong: 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
+  weekdaysShort: 'So._Mo._Di._Mi._Do._Fr._Sa.'.split('_'),
+};
