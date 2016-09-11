8e7445c9c465901b4a32af801552f973ab430f90
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-11 21:29:17 +0200

    Added remaining floor operations.

--- src/calculations/round.spec.js
+++ src/calculations/round.spec.js
@@ -1,19 +1,35 @@
 import { toIso, toFragments } from '../index';
 
-const floorSeconds = (isoString) => {
+export const floorSeconds = (isoString) => {
   const fragments = toFragments(isoString);
   return toIso({ ...fragments, second: Math.floor(fragments.second) });
 };
 
-const floorMinutes = (isoString) => {
+export const floorMinutes = (isoString) => {
   const fragments = toFragments(isoString);
   return toIso({ ...fragments, second: 0 });
 };
 
-const floorHours = (isoString) => {
+export const floorHours = (isoString) => {
   const fragments = toFragments(isoString);
   return toIso({ ...fragments, minute: 0, second: 0 });
 };
+
+export const floorDay = (isoString) => {
+  const fragments = toFragments(isoString);
+  return toIso({ ...fragments, hour: 0, minute: 0, second: 0 });
+};
+
+export const floorMonth = (isoString) => {
+  const fragments = toFragments(isoString);
+  return toIso({ ...fragments, day: 1, hour: 0, minute: 0, second: 0 });
+};
+
+export const floorYear = (isoString) => {
+  const fragments = toFragments(isoString);
+  return toIso({ ...fragments, month: 1, day: 1, hour: 0, minute: 0, second: 0 });
+};
+
 
 import { assertThat, equalTo } from 'hamjest';
 
@@ -24,13 +40,28 @@ describe.only('round', () => {
   });
 
   describe('floorMinutes', () => {
-    it('2000-01-01T11:12:13.123 results in 2000-01-01T11:00:00', () => assertThat(
+    it('2000-01-01T11:12:13.123 results in 2000-01-01T11:12:00', () => assertThat(
       floorMinutes('2000-01-01T11:12:13.123'), equalTo('2000-01-01T11:12:00')));
   });
 
   describe('floorHours', () => {
-    it('2000-01-01T11:12:13.123 results in 2000-01-01T00:00:00', () => assertThat(
+    it('2000-01-01T11:12:13.123 results in 2000-01-01T11:00:00', () => assertThat(
       floorHours('2000-01-01T11:12:13.123'), equalTo('2000-01-01T11:00:00')));
+  });
+
+  describe('floorDay', () => {
+    it('2000-01-01T11:12:13.123 results in 2000-01-01T00:00:00', () => assertThat(
+      floorDay('2000-01-01T11:12:13.123'), equalTo('2000-01-01T00:00:00')));
+  });
+
+  describe('floorMonth', () => {
+    it('2000-01-05T11:12:13.123 results in 2000-01-01T00:00:00', () => assertThat(
+      floorMonth('2000-01-05T11:12:13.123'), equalTo('2000-01-01T00:00:00')));
+  });
+
+  describe('floorYear', () => {
+    it('2000-02-05T11:12:13.123 results in 2000-01-01T00:00:00', () => assertThat(
+      floorYear('2000-02-05T11:12:13.123'), equalTo('2000-01-01T00:00:00')));
   });
 });
 
