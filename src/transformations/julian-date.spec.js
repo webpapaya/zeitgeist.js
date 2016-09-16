350690b3c5dfb75a81bd087e7c37af1b2043bd32
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-17 00:09:08 +0200

    Added tests for inversability of julian date fns.

--- src/transformations/julian-date.spec.js
+++ src/transformations/julian-date.spec.js
@@ -7,11 +7,19 @@ describe('toJulianDate', () => {
 
   it('ISO 2000-01-01T10:20:30 is 2451544.9309027777 in Julian date', () => assertThat(
     toJulianDate('2000-01-01T10:20:30'), equalTo(2451544.9309027777)));
+
+  it('is inverse of fromJulianDate', () => assertThat(
+    fromJulianDate(toJulianDate('2000-01-01')), equalTo('2000-01-01')));
 });
 
 describe('fromJulianDate', () => {
   it('julian date 2451544.5 is ISO 2000-01-01', () => assertThat(
     fromJulianDate(2451544.5), equalTo('2000-01-01')));
+
+  it('is inverse of toJulianDate', () => assertThat(
+    toJulianDate(fromJulianDate(2451544.5)), equalTo(2451544.5)));
 });
+
+
 
 
