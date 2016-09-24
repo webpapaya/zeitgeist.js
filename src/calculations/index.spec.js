b7b00aac99dab0da6567696612b4a4ae8fcfd722
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-24 07:24:32 +0200

    Replace first tests with simplified and automatically generated tests.

--- src/calculations/index.spec.js
+++ src/calculations/index.spec.js
@@ -5,7 +5,6 @@ import {
   addSeconds,
   addMinutes,
   addHours,
-
   addDays,
   addMonths,
   addYears,
@@ -13,7 +12,6 @@ import {
   subtractSeconds,
   subtractMinutes,
   subtractHours,
-
   subtractDays,
   subtractMonths,
   subtractYears,
@@ -28,116 +26,81 @@ describe('normalize', () => {
 });
 
 describe('addSeconds', () => {
-  it('adding 1 second to 2001-01-01T00:00:00 results in 2001-01-01T00:00:01', () => assertThat(
-    addSeconds('2001-01-01T00:00:00', 1), equalTo('2001-01-01T00:00:01')));
-
-  it('adding 2 seconds to 2001-01-01T00:00:00 results in 2001-01-01T00:00:02', () => assertThat(
-    addSeconds('2001-01-01T00:00:00', 2), equalTo('2001-01-01T00:00:02')));
-
-  it('adding 60 seconds to 2001-01-01T00:00:00 results in 2001-01-01T00:01:00', () => assertThat(
-    addSeconds('2001-01-01T00:00:00', 60), equalTo('2001-01-01T00:01:00')));
-
-  it('adding 1 seconds to 2001-01-01T00:59:59 results in 2001-01-01T01:00:00', () => assertThat(
-    addSeconds('2001-01-01T00:59:59', 1), equalTo('2001-01-01T01:00:00')));
-
-  it('adding 1 seconds to 2001-01-01T00:59:58 results in 2001-01-01T00:59:59', () => assertThat(
-    addSeconds('2001-01-01T00:59:58', 1), equalTo('2001-01-01T00:59:59')));
-
-  it('adding 1 seconds to 2001-01-01T23:59:59 results in 2001-01-02T00:00:00', () => assertThat(
-    addSeconds('2001-01-01T23:59:59', 1), equalTo('2001-01-02T00:00:00')));
-
-  it('adding 1 seconds to 2001-01-01T23:59:58 results in 2001-01-01T23:59:59', () => assertThat(
-    addSeconds('2001-01-01T23:59:58', 1), equalTo('2001-01-01T23:59:59')));
+  [
+    { date: '2001-01-01T00:00:00', plusSeconds: 1, resultsIn: '2001-01-01T00:00:01' },
+    { date: '2001-01-01T00:59:59', plusSeconds: 1, resultsIn: '2001-01-01T01:00:00' },
+    { date: '2001-01-01T00:59:58', plusSeconds: 1, resultsIn: '2001-01-01T00:59:59' },
+    { date: '2001-01-01T23:59:59', plusSeconds: 1, resultsIn: '2001-01-02T00:00:00' },
+    { date: '2001-01-01T00:00:00', plusSeconds: 2, resultsIn: '2001-01-01T00:00:02' },
+    { date: '2001-01-01T00:00:00', plusSeconds: 60, resultsIn: '2001-01-01T00:01:00' },
+  ].forEach(({ date, plusSeconds, resultsIn }) => {
+    it(`${plusSeconds} to ${date} results in ${resultsIn}`, () => assertThat(
+      addSeconds(date, plusSeconds), equalTo(resultsIn)));
+  });
 });
 
 describe('addMinutes', () => {
-  it('adding 1 minute to 2001-01-01T00:00:00 results in 2001-01-01T00:01:00', () => assertThat(
-    addMinutes('2001-01-01T00:00:00', 1), equalTo('2001-01-01T00:01:00')));
-
-  it('adding 1 minute to 2001-01-01T00:59:00 results in 2001-01-01T01:00:00', () => assertThat(
-    addMinutes('2001-01-01T00:59:00', 1), equalTo('2001-01-01T01:00:00')));
-
-  it('adding -1 minute to 2001-01-01T01:00:00 results in 2001-01-01T00:59:00', () => assertThat(
-    addMinutes('2001-01-01T01:00:00', -1), equalTo('2001-01-01T00:59:00')));
-
-  it('adding 0.5 minutes to 2001-01-01T00:00:00 results in 2001-01-01T00:00:30', () => assertThat(
-    addMinutes('2001-01-01T00:00:00', 0.5), equalTo('2001-01-01T00:00:30')));
+  [
+    { date: '2001-01-01T01:00:00', plusMinutes: -1, resultsIn: '2001-01-01T00:59:00' },
+    { date: '2001-01-01T00:00:00', plusMinutes: 0.5, resultsIn: '2001-01-01T00:00:30' },
+    { date: '2001-01-01T00:00:00', plusMinutes: 1, resultsIn: '2001-01-01T00:01:00' },
+    { date: '2001-01-01T00:59:00', plusMinutes: 1, resultsIn: '2001-01-01T01:00:00' },
+  ].forEach(({ date, plusMinutes, resultsIn }) => {
+    it(`${plusMinutes} to ${date} results in ${resultsIn}`, () => assertThat(
+      addMinutes(date, plusMinutes), equalTo(resultsIn)));
+  });
 });
 
 describe('addHours', () => {
-  it('adding 1 hour to 2001-01-01T00:00:00 results in 2001-01-01T01:00:00', () => assertThat(
+  it('1 hour to 2001-01-01T00:00:00 results in 2001-01-01T01:00:00', () => assertThat(
     addHours('2001-01-01T00:00:00', 1), equalTo('2001-01-01T01:00:00')));
 
-  it('adding -1 hour to 2001-01-01T01:00:00 results in 2001-01-01T00:00:00', () => assertThat(
+  it('-1 hour to 2001-01-01T01:00:00 results in 2001-01-01T00:00:00', () => assertThat(
     addHours('2001-01-01T01:00:00', -1), equalTo('2001-01-01T00:00:00')));
 });
 
 describe('addDays', () => {
-  it('adding 1 day to 2000-01-01T00:00:00 results in 2000-01-02', () => assertThat(
-    addDays('2000-01-01T00:00:00', 1), equalTo('2000-01-02T00:00:00')));
-
-  it('adding 31 days to 2000-01-01T00:00:00 results in 2000-02-01T00:00:00', () => assertThat(
-    addDays('2000-01-01T00:00:00', 31), equalTo('2000-02-01T00:00:00')));
-
-  it('adding 31 + 29 days to 2000-01-01 results in 2000-03-01', () => assertThat(
-    addDays('2000-01-01T00:00:00', 60), equalTo('2000-03-01T00:00:00')));
-
-  it('adding 1 day to 2000-01-31 results in 2000-02-01', () => assertThat(
-    addDays('2000-01-31T00:00:00', 1), equalTo('2000-02-01T00:00:00')));
-
-  it('adding 2 days to 2000-01-31 results in 2000-02-02', () => assertThat(
-    addDays('2000-01-31T00:00:00', 2), equalTo('2000-02-02T00:00:00')));
-
-  it('adding 365 days to 2000-01-01 results in 2001-01-01', () => assertThat(
-    addDays('2000-01-01T00:00:00', 366), equalTo('2001-01-01T00:00:00')));
-
-  it('adding -1 day to 2001-01-01 results in 2000-12-31', () => assertThat(
-    addDays('2001-01-01T00:00:00', -1), equalTo('2000-12-31T00:00:00')));
+  [
+    { date: '2000-01-01T00:00:00', plusDays: 1, resultsIn: '2000-01-02T00:00:00' },
+    { date: '2000-01-01T00:00:00', plusDays: 31, resultsIn: '2000-02-01T00:00:00' },
+    { date: '2000-01-01T00:00:00', plusDays: 31 + 29, resultsIn: '2000-03-01T00:00:00' },
+    { date: '2000-01-31T00:00:00', plusDays: 1, resultsIn: '2000-02-01T00:00:00' },
+    { date: '2000-01-31T00:00:00', plusDays: 2, resultsIn: '2000-02-02T00:00:00' },
+    { date: '2000-01-01T00:00:00', plusDays: 366, resultsIn: '2001-01-01T00:00:00' },
+    { date: '2001-01-01T00:00:00', plusDays: 365, resultsIn: '2002-01-01T00:00:00' },
+    { date: '2001-01-01T00:00:00', plusDays: -1, resultsIn: '2000-12-31T00:00:00' },
+  ].forEach(({ date, plusDays, resultsIn }) => {
+    it(`${plusDays} to ${date} results in ${resultsIn}`, () => assertThat(
+      addDays(date, plusDays), equalTo(resultsIn)));
+  });
 });
 
 describe('addMonths', () => {
-  it('adding -1 months to 2000-01-01 results in 1999-12-01', () => assertThat(
-    addMonths('2000-01-01', -1), equalTo('1999-12-01')));
-
-  it('adding 0 months to 2000-01-01 results in 2000-01-01', () => assertThat(
-    addMonths('2000-01-01', 0), equalTo('2000-01-01')));
-
-  it('adding 1 month to 2000-01-01 results in 2000-02-01', () => assertThat(
-    addMonths('2000-01-01', 1), equalTo('2000-02-01')));
-
-  it('adding 1 month to 2000-02-01 results in 2000-03-01', () => assertThat(
-    addMonths('2000-02-01', 1), equalTo('2000-03-01')));
-
-  it('adding 12 months to 2000-01-01 results in 2001-01-01', () => assertThat(
-    addMonths('2000-01-01', 12), equalTo('2001-01-01')));
-
-  it('adding 13 months to 2000-01-01 results in 2001-02-01', () => assertThat(
-    addMonths('2000-01-01', 13), equalTo('2001-02-01')));
-
-  it('adding -13 months to 2001-02-01 results in 2000-01-01', () => assertThat(
-    addMonths('2001-02-01', -13), equalTo('2000-01-01')));
-
-  it('adding 14 months to 2000-01-01 results in 2001-03-01', () => assertThat(
-    addMonths('2000-01-01', 14), equalTo('2001-03-01')));
-
-  it('adding 24 months to 2000-01-01 results in 2002-01-01', () => assertThat(
-    addMonths('2000-01-02', 24), equalTo('2002-01-02')));
-
-  it('adding 12000 months (1000 years) to 2000-01-01 results in 3000-01-01', () => assertThat(
-    addMonths('2000-01-01', 12 * 1000), equalTo('3000-01-01')));
-
-  it('adding -1 months to 2000-02-01 results in 2000-01-01', () => assertThat(
-    addMonths('2000-02-01', -1), equalTo('2000-01-01')));
+  [
+    { date: '2001-02-01', plusMonths: -13, resultsIn: '2000-01-01' },
+    { date: '2000-01-01', plusMonths: -1, resultsIn: '1999-12-01' },
+    { date: '2000-02-01', plusMonths: -1, resultsIn: '2000-01-01' },
+    { date: '2000-01-01', plusMonths: 0, resultsIn: '2000-01-01' },
+    { date: '2000-01-01', plusMonths: 1, resultsIn: '2000-02-01' },
+    { date: '2000-01-01', plusMonths: 12, resultsIn: '2001-01-01' },
+    { date: '2000-01-01', plusMonths: 13, resultsIn: '2001-02-01' },
+    { date: '2000-01-01', plusMonths: 14, resultsIn: '2001-03-01' },
+    { date: '2000-01-01', plusMonths: 24, resultsIn: '2002-01-01' },
+    { date: '2000-01-01', plusMonths: 12 * 1000, resultsIn: '3000-01-01' },
+  ].forEach(({ date, plusMonths, resultsIn }) => {
+    it(`${plusMonths} to ${date} results in ${resultsIn}`, () => assertThat(
+      addMonths(date, plusMonths), equalTo(resultsIn)));
+  });
 });
 
 describe('addYears', () => {
-  it('adding 1 year to 2000-01-01 results in 2001-01-01', () => assertThat(
+  it('1 to 2000-01-01 results in 2001-01-01', () => assertThat(
     addYears('2000-01-01', 1), equalTo('2001-01-01')));
 
-  it('adding 10 year to 2000-01-01 results in 2010-01-01', () => assertThat(
+  it('10 to 2000-01-01 results in 2010-01-01', () => assertThat(
     addYears('2000-01-01', 10), equalTo('2010-01-01')));
 
-  it('add -1 years to 2001-01-01 results in 2000-01-01', () => assertThat(
+  it('-1 to 2001-01-01 results in 2000-01-01', () => assertThat(
     addYears('2001-01-01', -1), equalTo('2000-01-01')));
 });
 
