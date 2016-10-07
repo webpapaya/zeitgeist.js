4984e27e2c62ef7999534a38ed43413ee739849b
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-07 18:34:09 +0200

    Use regex builder.

--- src/datetime/validate.js
+++ src/datetime/validate.js
@@ -2,7 +2,7 @@ import { createRegexBuilder } from '../utils';
 import { INVALID_FORMAT } from '../core/constants';
 
 
-const MATCH_YEAR = /[+-]?\d+\b/.source;
+const MATCH_YEAR = /[+-]?\d+/.source;
 const MATCH_MONTH = /[+-]?\d{2}/.source;
 const MATCH_DAY = /[+-]?\d{2}/.source;
 
@@ -13,12 +13,25 @@ const MATCH_SECOND = /[+-]?\d{2}(\.\d+)?/.source;
 const MATCH_TIMEZONE = /[+-]\d{2}(:\d{2})?|Z/.source;
 const MATCH_TIME_SEPARATOR = /[\sT]/.source;
 
-const MATCH_YEAR_MONTH_DAY = `${MATCH_YEAR}-${MATCH_MONTH}-${MATCH_DAY}`;
-const MATCH_HOUR_MINUTE_SECOND = `${MATCH_HOUR}:${MATCH_MINUTE}:${MATCH_SECOND}`;
+const MATCH_YEAR_MONTH_DAY = createRegexBuilder()
+  .and(MATCH_YEAR)
+  .and('-')
+  .and(MATCH_MONTH)
+  .and('-')
+  .and(MATCH_DAY)
+  .toValue();
+
+const MATCH_HOUR_MINUTE_SECOND = createRegexBuilder()
+  .and(MATCH_HOUR)
+  .and(':')
+  .and(MATCH_MINUTE)
+  .and(':')
+  .and(MATCH_SECOND)
+  .toValue();
+
 const MATCH_DATE_AND_TIME = `${MATCH_YEAR_MONTH_DAY}${MATCH_TIME_SEPARATOR}${MATCH_HOUR_MINUTE_SECOND}`;
 
-
-const xxx = createRegexBuilder()
+const THE_MOTHER_OF_ISO8601_DATE_TIME = createRegexBuilder()
   .or(`^${MATCH_YEAR}$`)
   .or(`^${MATCH_YEAR}-${MATCH_MONTH}$`)
   .or(`^${MATCH_YEAR}-${MATCH_MONTH}-${MATCH_DAY}$`)
@@ -27,9 +40,7 @@ const xxx = createRegexBuilder()
   .or(`^${MATCH_YEAR_MONTH_DAY}${MATCH_TIME_SEPARATOR}${MATCH_HOUR}:${MATCH_MINUTE}:${MATCH_SECOND}$`)
   .or(`^${MATCH_YEAR_MONTH_DAY}${MATCH_TIME_SEPARATOR}${MATCH_HOUR}:${MATCH_MINUTE}${MATCH_TIMEZONE}$`)
   .or(`^${MATCH_YEAR_MONTH_DAY}${MATCH_TIME_SEPARATOR}${MATCH_HOUR}${MATCH_TIMEZONE}$`)
-  .or(`^${MATCH_DATE_AND_TIME}${MATCH_TIMEZONE}`)
-
-const THE_MOTHER_OF_ISO8601_DATE_TIME = xxx;
+  .or(`^${MATCH_DATE_AND_TIME}${MATCH_TIMEZONE}`);
 
 export const isValid = (isoString) => THE_MOTHER_OF_ISO8601_DATE_TIME.test(isoString);
 
