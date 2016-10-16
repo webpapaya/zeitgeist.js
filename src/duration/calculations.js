3290477e14158455ece9a7fb59dfe5a2fdc0c7ed
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-16 15:20:26 +0200

    Extracted normalize fn.

--- src/duration/calculations.js
+++ src/duration/calculations.js
@@ -1,43 +1,6 @@
-import {
-  addMicroseconds,
-  addMilliseconds,
-  addSeconds,
-  addMinutes,
-  addHours,
-  addDays,
-  addWeeks,
-  addMonths,
-  addYears,
-} from './calculations/add';
-
-import {
-  ONE_HOUR,
-  ONE_MINUTE,
-  ONE_SECOND,
-} from './constants';
-
-import {
-  toIso,
-  toFragments,
-  asMicroseconds,
-  removeDateComponent,
-} from './index';
-
-export const normalize = (isoString) => {
-  const fragments = toFragments(isoString);
-
-  const microseconds = asMicroseconds(removeDateComponent(isoString));
-  const hours = Math.floor(microseconds / ONE_HOUR);
-  const minutes = Math.floor((microseconds - hours * ONE_HOUR) / ONE_MINUTE);
-  const seconds = ((microseconds - minutes * ONE_MINUTE) - (hours * ONE_HOUR)) / ONE_SECOND;
-
-  return toIso({
-    ...fragments,
-    hours,
-    minutes,
-    seconds,
-  });
-};
+export {
+  normalize
+} from './calculations/normalize';
 
 export {
   addMicroseconds,
