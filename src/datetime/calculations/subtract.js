6bb200726c002eac82fb2d7012f3d094f94c72bd
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-16 14:21:03 +0200

    Curry subtract fns.

--- src/datetime/calculations/subtract.js
+++ src/datetime/calculations/subtract.js
@@ -1,3 +1,4 @@
+import { curry, pipe } from 'ramda';
 import {
   toFragments as toDurationFragments,
 } from '../../duration/index';
@@ -13,30 +14,23 @@ import {
   addYears,
 } from '../index';
 
-const _addMonths = (months) => (isoString) => addMonths(months, isoString);
-const _addYears = (years) => (isoString) => addYears(years, isoString);
-const _addDays = (days) => (isoString) => addDays(days, isoString);
-const _addHours = (hours) => (isoString) => addHours(hours, isoString);
-const _addMinutes = (minutes) => (isoString) => addMinutes(minutes, isoString);
-const _addSeconds = (seconds) => (isoString) => addSeconds(seconds, isoString);
-
 export const subtractDuration = (isoString, isoDuration) => {
   const { years, months, days, hours, minutes, seconds } = toDurationFragments(isoDuration);
 
-  return compose(
-    _addDays(days * -1),
-    _addMonths(months * -1),
-    _addYears(years * -1),
-    _addHours(hours * -1),
-    _addMinutes(minutes * -1),
-    _addSeconds(seconds * -1),
+  return pipe(
+    addDays(days * -1),
+    addMonths(months * -1),
+    addYears(years * -1),
+    addHours(hours * -1),
+    addMinutes(minutes * -1),
+    addSeconds(seconds * -1),
   )(isoString);
 };
 
-export const subtractSeconds = (seconds, isoString) => addSeconds(seconds * -1, isoString);
-export const subtractMinutes = (minutes, isoString) => addMinutes(minutes * -1, isoString);
-export const subtractHours = (hours, isoString) => addHours(hours * -1, isoString);
+export const subtractSeconds = curry((seconds, isoString) => addSeconds(seconds * -1, isoString));
+export const subtractMinutes = curry((minutes, isoString) => addMinutes(minutes * -1, isoString));
+export const subtractHours = curry((hours, isoString) => addHours(hours * -1, isoString));
 
-export const subtractDays = (days, isoString) => addDays(days * -1, isoString);
-export const subtractMonths = (months, isoString) => addMonths(months * -1, isoString);
-export const subtractYears = (years, isoString) => addYears(years * -1, isoString);
+export const subtractDays = curry((days, isoString) => addDays(days * -1, isoString));
+export const subtractMonths = curry((months, isoString) => addMonths(months * -1, isoString));
+export const subtractYears = curry((years, isoString) => addYears(years * -1, isoString));
