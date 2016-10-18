e949e8f4d39a60ef5f203f6dd5edc5dc4b69878b
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-18 19:46:32 +0200

    Added remaining ceil fns.

--- src/duration/calculations/ceil.spec.js
+++ src/duration/calculations/ceil.spec.js
@@ -22,9 +22,11 @@ export const ceilSecond = (isoDuration) => floorSecond(addSeconds(1, isoDuration
 export const ceilMinute = (isoDuration) => floorMinute(addMinutes(1, isoDuration));
 export const ceilHour = (isoDuration) => floorHour(addHours(1, isoDuration));
 export const ceilDay = (isoDuration) => floorDay(addDays(1, isoDuration));
+export const ceilWeek = (isoDuration) => floorWeek(addWeeks(1, isoDuration));
+export const ceilMonth = (isoDuration) => floorMonth(addMonths(1, isoDuration));
+export const ceilYear = (isoDuration) => floorYear(addYears(1, isoDuration));
 
 import { assertThat, equalTo } from 'hamjest';
-
 describe('ceil', () => {
   describe('ceilSecond', () => {
     it('PT1.123S results in PT2S', () => assertThat(
@@ -46,5 +48,18 @@ describe('ceil', () => {
       ceilDay('P1DT1H1M1.123S'), equalTo('P2D')));
   });
 
-  
+  describe('ceilWeek', () => {
+    it('P1W1DT1H1M1.123S results in P2D', () => assertThat(
+      ceilWeek('P1W1DT1H1M1.123S'), equalTo('P2W')));
+  });
+
+  describe('ceilMonth', () => {
+    it('P1M1W1DT1H1M1.123S results in P2M', () => assertThat(
+      ceilMonth('P1M1W1DT1H1M1.123S'), equalTo('P2M')));
+  });
+
+  describe('ceilYear', () => {
+    it('P1Y1M1W1DT1H1M1.123S results in P2Y', () => assertThat(
+      ceilYear('P1Y1M1W1DT1H1M1.123S'), equalTo('P2Y')));
+  });
 });
