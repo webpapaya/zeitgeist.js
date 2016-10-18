864539306b81db012d53d81a13dae0125be8c7c4
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-18 08:33:30 +0200

    Extracted floor fns into own file.

---
+++ src/duration/calculations/floor.js
@@ -0,0 +1,60 @@
+import {
+  toIso,
+  toFragments,
+} from '../index';
+
+const prepareArg = (fn) => (isoDuration) => toIso(fn(toFragments(isoDuration)));
+
+export const floorSecond = prepareArg((fragments) => ({
+  ...fragments, seconds: Math.floor( fragments.seconds )
+}));
+
+export const floorMinute = prepareArg((fragments) => ({
+  ...fragments,
+  minutes: Math.floor(fragments.minutes),
+  seconds: 0,
+}));
+
+export const floorHour = prepareArg((fragments) => ({
+  hours: Math.floor(fragments.hours),
+  minutes: 0,
+  seconds: 0,
+}));
+
+export const floorDay = prepareArg((fragments) => ({
+  ...fragments,
+  days: Math.floor(fragments.days),
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+}));
+
+export const floorWeek = prepareArg((fragments) => ({
+  ...fragments,
+  weeks:  Math.floor(fragments.weeks),
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+}));
+
+export const floorMonth = prepareArg((fragments) => ({
+  ...fragments,
+  months: Math.floor(fragments.months),
+  weeks: 0,
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+}));
+
+export const floorYear = prepareArg((fragments) => ({
+  ...fragments,
+  year: Math.floor(fragments.months),
+  months: 0,
+  weeks: 0,
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+}));
--- src/duration/calculations/round.spec.js
+++ src/duration/calculations/round.spec.js
@@ -1,68 +1,15 @@
-import {
-  toIso,
-  toFragments,
-} from '../index';
-
-const prepareArg = (fn) => (isoDuration) => toIso(fn(toFragments(isoDuration)));
-
-const floorSecond = prepareArg((fragments) => ({
-  ...fragments, seconds: Math.floor( fragments.seconds )
-}));
-
-const floorMinute = prepareArg((fragments) => ({
-  ...fragments,
-  minutes: Math.floor(fragments.minutes),
-  seconds: 0,
-}));
-
-const floorHour = prepareArg((fragments) => ({
-  hours: Math.floor(fragments.hours),
-  minutes: 0,
-  seconds: 0,
-}));
-
-const floorDay = prepareArg((fragments) => ({
-  ...fragments,
-  days: Math.floor(fragments.days),
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-}));
-
-const floorWeek = prepareArg((fragments) => ({
-  ...fragments,
-  weeks:  Math.floor(fragments.weeks),
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-}));
-
-const floorMonth = prepareArg((fragments) => ({
-  ...fragments,
-  months: Math.floor(fragments.months),
-  weeks: 0,
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-}));
-
-const floorYear = prepareArg((fragments) => ({
-  ...fragments,
-  year: Math.floor(fragments.months),
-  months: 0,
-  weeks: 0,
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-}));
-
-
 import { assertThat, equalTo } from 'hamjest';
+import {
+  floorSecond,
+  floorMinute,
+  floorHour,
+  floorDay,
+  floorWeek,
+  floorMonth,
+  floorYear,
+} from './floor';
 
-describe.only('floor', () => {
+describe('floor', () => {
   describe('floorSecond', () => {
     it('PT1.123S results in PT1S', () => assertThat(
       floorSecond('PT1.123S'), equalTo('PT1S')));
