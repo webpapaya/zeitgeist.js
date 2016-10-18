033de0437acf3bc0a8f47be74c3324c8092f19fb
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-18 20:05:22 +0200

    Extracted toIso.

--- src/datetime/calculations/floor.js
+++ src/datetime/calculations/floor.js
@@ -6,27 +6,33 @@ import {
   subtractDays,
 } from '../index';
 
-const parseArgAsFragments = (fn) => (isoString) => fn(toFragments(isoString));
+const parseArgAsFragments = (fn) => (isoString) => toIso(fn(toFragments(isoString)));
 
-export const floorSecond = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, second: Math.floor(fragments.second) }));
+export const floorSecond = parseArgAsFragments((fragments) => ({
+  ...fragments, second: Math.floor(fragments.second)
+}));
 
-export const floorMinute = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, second: 0 }));
+export const floorMinute = parseArgAsFragments((fragments) => ({
+  ...fragments, second: 0
+}));
 
-export const floorHour = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, minute: 0, second: 0 }));
+export const floorHour = parseArgAsFragments((fragments) => ({
+  ...fragments, minute: 0, second: 0
+}));
 
 export const floorWeek = (isoString) => {
   const weekDay = getWeekday(isoString);
   return floorDay(subtractDays(weekDay - 1, isoString));
 };
 
-export const floorDay = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, hour: 0, minute: 0, second: 0 }));
+export const floorDay = parseArgAsFragments((fragments) => ({
+  ...fragments, hour: 0, minute: 0, second: 0
+}));
 
-export const floorMonth = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, day: 1, hour: 0, minute: 0, second: 0 }));
+export const floorMonth = parseArgAsFragments((fragments) => ({
+  ...fragments, day: 1, hour: 0, minute: 0, second: 0
+}));
 
-export const floorYear = parseArgAsFragments((fragments) =>
-  toIso({ ...fragments, month: 1, day: 1, hour: 0, minute: 0, second: 0 }));
+export const floorYear = parseArgAsFragments((fragments) => ({
+  ...fragments, month: 1, day: 1, hour: 0, minute: 0, second: 0
+}));
