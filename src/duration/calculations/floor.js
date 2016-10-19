c6fad5af688a7417c10f68647183e7e49d2017a6
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-19 08:37:31 +0200

    Use constants as identifier to prevent spelling errors.

--- src/duration/calculations/floor.js
+++ src/duration/calculations/floor.js
@@ -3,7 +3,10 @@ import {
   toFragments,
 } from '../index';
 
+import { UNIT_NAMES } from '../constants';
+
 const prepareArg = (fn) => (isoDuration) => toIso(fn(toFragments(isoDuration)));
+const { seconds, minutes, hours, days, weeks, months, years } = UNIT_NAMES;
 
 export const floorSecond = prepareArg((fragments) => ({
   ...fragments, seconds: Math.floor(fragments.seconds),
@@ -11,50 +14,50 @@ export const floorSecond = prepareArg((fragments) => ({
 
 export const floorMinute = prepareArg((fragments) => ({
   ...fragments,
-  minutes: Math.floor(fragments.minutes),
-  seconds: 0,
+  [minutes]: Math.floor(fragments.minutes),
+  [seconds]: 0,
 }));
 
 export const floorHour = prepareArg((fragments) => ({
-  hours: Math.floor(fragments.hours),
-  minutes: 0,
-  seconds: 0,
+  [hours]: Math.floor(fragments.hours),
+  [minutes]: 0,
+  [seconds]: 0,
 }));
 
 export const floorDay = prepareArg((fragments) => ({
   ...fragments,
-  days: Math.floor(fragments.days),
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
+  [days]: Math.floor(fragments.days),
+  [hours]: 0,
+  [minutes]: 0,
+  [seconds]: 0,
 }));
 
 export const floorWeek = prepareArg((fragments) => ({
   ...fragments,
-  weeks: Math.floor(fragments.weeks),
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
+  [weeks]: Math.floor(fragments.weeks),
+  [days]: 0,
+  [hours]: 0,
+  [minutes]: 0,
+  [seconds]: 0,
 }));
 
 export const floorMonth = prepareArg((fragments) => ({
   ...fragments,
-  months: Math.floor(fragments.months),
-  weeks: 0,
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
+  [months]: Math.floor(fragments.months),
+  [weeks]: 0,
+  [days]: 0,
+  [hours]: 0,
+  [minutes]: 0,
+  [seconds]: 0,
 }));
 
 export const floorYear = prepareArg((fragments) => ({
   ...fragments,
-  year: Math.floor(fragments.months),
-  months: 0,
-  weeks: 0,
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
+  [years]: Math.floor(fragments.months),
+  [months]: 0,
+  [weeks]: 0,
+  [days]: 0,
+  [hours]: 0,
+  [minutes]: 0,
+  [seconds]: 0,
 }));
