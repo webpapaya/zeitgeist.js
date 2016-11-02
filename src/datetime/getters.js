e22ecdaaf1668112ec5c298f4ce21fd296b502e5
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-11-02 08:31:08 +0100

    Extracted fns from index.js into getters.js

--- src/datetime/getters.js
+++ src/datetime/getters.js
@@ -1,4 +1,15 @@
-import { daysBetween, toFragments, toIso } from './index';
+import {
+  daysBetween,
+  toFragments,
+  toIso,
+  removeTimeComponent,
+  removeDateComponent
+} from './index';
+import { isEmpty } from '../utils';
+import {
+  FEBRUARY,
+  DAYS_IN_MONTHS,
+} from './constants';
 
 const WEEKDAY_REFERENCE_DATE = '2001-01-01';
 const DAYS_IN_ONE_WEEK = 7;
@@ -34,3 +45,38 @@ export const getDay = parseIsoString(({ day }) => day);
 export const getHour = parseIsoString(({ hour }) => hour);
 export const getMinute = parseIsoString(({ minute }) => minute);
 export const getSecond = parseIsoString(({ second }) => second);
+
+const isLeapMonth = (isoString, month) =>
+  month === FEBRUARY && isLeapYear(isoString);
+
+export const isLeapYear = (isoString) => {
+  const { year } = toFragments(isoString);
+  const dividableBy4 = year % 4 === 0;
+  const dividableBy100 = year % 100 === 0;
+  const dividableBy400 = year % 400 === 0;
+
+  return (dividableBy4 && !dividableBy100) || dividableBy400;
+};
+
+
+export const daysInYear = (isoString) => isLeapYear(isoString) ? 366 : 365;
+
+export const daysInMonth = (isoString) => {
+  const { month = 1 } = toFragments(isoString);
+  if (isLeapMonth(isoString, month)) { return 29; }
+  return DAYS_IN_MONTHS[month];
+};
+
+export const isLastDayOfMonth = (isoString) => {
+  const { day } = toFragments(isoString);
+  return daysInMonth(isoString) === day;
+};
+
+export const isFirstDayOfMonth = (isoString) => {
+  const { day } = toFragments(isoString);
+  return day === 1;
+};
+
+export const containsDateComponent = (isoString) => !isEmpty(removeTimeComponent(isoString));
+export const containsTimeComponent = (isoString) => !isEmpty(removeDateComponent(isoString));
+
