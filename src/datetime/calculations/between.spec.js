0c5df22317084a9e9d71f62779d3c1fd993ed4f2
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-12-04 20:27:03 +0100

    Add additional test for minutes.

--- src/datetime/calculations/between.spec.js
+++ src/datetime/calculations/between.spec.js
@@ -54,6 +54,9 @@ describe('minutes between', () => {
   it('T10:00 and T11:00 is 60 minutes', () => assertThat(
     minutesBetween('T11:00', 'T10:00'), equalTo(60)));
 
+  it('2000-01-01T10:00:00+00:00 and 2000-01-01T10:00:00+00:15 is 15 minutes', () => assertThat(
+    minutesBetween('2000-01-01T10:00:00+00:00', '2000-01-01T10:00:00+00:15'), equalTo(15)));
+
   it('can be curried', () => assertThat(
     minutesBetween('T11:00')('T10:00'), equalTo(60)));
 });
