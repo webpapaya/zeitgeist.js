479eb8a1fc6b158bdd8c2e06e179505192cf1bfd
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-12-13 22:37:27 +0100

    Start of fns work with timezones as well.

--- src/datetime/calculations/start-of.spec.js
+++ src/datetime/calculations/start-of.spec.js
@@ -14,6 +14,9 @@ describe('startOfSecond', () => {
   it('2000-01-01T10:11:12.12 results in 2000-01-01T10:11:12', () => assertThat(
     startOfSecond('2000-01-01T10:11:12.12'), equalTo('2000-01-01T10:11:12')));
 
+  it('2000-01-01T10:11:12.12+10:00 results in 2000-01-01T10:11:12+10:00', () => assertThat(
+    startOfSecond('2000-01-01T10:11:12.12+10:00'), equalTo('2000-01-01T10:11:12+10:00')));
+
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfSecond('I\'m invalid'), equalTo(INVALID_DATETIME)));
 });
@@ -21,6 +24,9 @@ describe('startOfSecond', () => {
 describe('startOfMinute', () => {
   it('2000-01-01T10:11:12.12 results in 2000-01-01T10:11:00', () => assertThat(
     startOfMinute('2000-01-01T10:11:12.12'), equalTo('2000-01-01T10:11:00')));
+
+  it('2000-01-01T10:11:12.12+10:00 results in 2000-01-01T10:11:00+10:00', () => assertThat(
+    startOfMinute('2000-01-01T10:11:12.12+10:00'), equalTo('2000-01-01T10:11:00+10:00')));
 
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfMinute('I\'m invalid'), equalTo(INVALID_DATETIME)));
@@ -30,6 +36,9 @@ describe('startOfHour', () => {
   it('2000-01-01T10:11:12.12 results in 2000-01-01T10:00:00', () => assertThat(
     startOfHour('2000-01-01T10:11:12.12'), equalTo('2000-01-01T10:00:00')));
 
+  it('2000-01-01T10:11:12.12+10:00 results in 2000-01-01T10:00:00+10:00', () => assertThat(
+    startOfHour('2000-01-01T10:11:12.12+10:00'), equalTo('2000-01-01T10:00:00+10:00')));
+
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfHour('I\'m invalid'), equalTo(INVALID_DATETIME)));
 });
@@ -37,6 +46,9 @@ describe('startOfHour', () => {
 describe('startOfDay', () => {
   it('2000-01-01T10:11:12.12 results in 2000-01-01T00:00:00', () => assertThat(
     startOfDay('2000-01-01T10:11:12.12'), equalTo('2000-01-01T00:00:00')));
+
+  it('2000-01-01T10:11:12.12+10:00 results in 2000-01-01T00:00:00+10:00', () => assertThat(
+    startOfDay('2000-01-01T10:11:12.12+10:00'), equalTo('2000-01-01T00:00:00+10:00')));
 
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfDay('I\'m invalid'), equalTo(INVALID_DATETIME)));
@@ -46,6 +58,9 @@ describe('startOfWeek', () => {
   it('2000-01-01T10:11:12.12 results in 1999-12-27T00:00:00', () => assertThat(
     startOfWeek('2000-01-01T10:11:12.12'), equalTo('1999-12-27T00:00:00')));
 
+  it('2000-01-01T10:11:12.12+10:00 results in 1999-12-27T00:00:00+10:00', () => assertThat(
+    startOfWeek('2000-01-01T10:11:12.12+10:00'), equalTo('1999-12-27T00:00:00+10:00')));
+
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfWeek('I\'m invalid'), equalTo(INVALID_DATETIME)));
 });
@@ -54,6 +69,9 @@ describe('startOfMonth', () => {
   it('2000-01-02T10:11:12.12 results in 2000-01-01T00:00:00', () => assertThat(
     startOfMonth('2000-01-02T10:11:12.12'), equalTo('2000-01-01T00:00:00')));
 
+  it('2000-01-02T10:11:12.12+10:00 results in 2000-01-01T00:00:00+10:00', () => assertThat(
+    startOfMonth('2000-01-02T10:11:12.12+10:00'), equalTo('2000-01-01T00:00:00+10:00')));
+
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfMonth('I\'m invalid'), equalTo(INVALID_DATETIME)));
 });
@@ -61,6 +79,9 @@ describe('startOfMonth', () => {
 describe('startOfYear', () => {
   it('2000-02-02T10:11:12.12 results in 2000-01-01T00:00:00', () => assertThat(
     startOfYear('2000-02-02T10:11:12.12'), equalTo('2000-01-01T00:00:00')));
+
+  it('2000-02-02T10:11:12.12+10:00 results in 2000-01-01T00:00:00+10:00', () => assertThat(
+    startOfYear('2000-02-02T10:11:12.12+10:00'), equalTo('2000-01-01T00:00:00+10:00')));
 
   it('`I\'m invalid`results in `INVALID_DATETIME`', () => assertThat(
     startOfYear('I\'m invalid'), equalTo(INVALID_DATETIME)));
