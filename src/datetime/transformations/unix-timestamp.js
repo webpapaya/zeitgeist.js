61a8efbcc52dae6b8e4d5f38ecf49d51a3fcd335
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-05-30 00:21:40 +0200

    Extracted unix timestamp fn to own file.

--- src/datetime/transformations/unix-timestamp.js
+++ src/datetime/transformations/unix-timestamp.js
@@ -1,12 +1,53 @@
+
+import { toFragments } from '../index';
+import { compose } from '../../utils';
 import { isValid } from '../validate';
 import { INVALID_DATETIME } from '../constants';
 import {
   fromUnixTimestamp as _fromUnixTimestamp,
-  toUnixTimestamp as _toUnixTimestamp,
+
 } from './unix-timestamp.internal';
 
 export const fromUnixTimestamp = _fromUnixTimestamp;
-export const toUnixTimestamp = (isoDateTime) => {
-  if (!isValid(isoDateTime)) { return INVALID_DATETIME; }
-  return _toUnixTimestamp(isoDateTime);
+
+
+const floor = (value) => Math.floor(value);
+export const daysSinceEpoch = ({ year: _year, month: m, day: d }) => {
+  const year = m <= 2
+      ? _year - 1
+      : _year
+    ;
+
+  const era = floor((year >= 0
+        ? year
+        : year - 399
+    ) / 400);
+
+  const yearOfEra = year - era * 400;
+  const dayOfYear = floor((153 * (m + (m > 2 ? -3 : 9)) + 2) / 5) + d - 1;
+  const dayOfEra = yearOfEra * 365 + floor(yearOfEra / 4) - floor(yearOfEra / 100) + dayOfYear;
+
+  return era * 146097 + dayOfEra - 719468;
+};
+
+const isFloat = (value) => !isNaN(value) && value.toString().indexOf('.') != -1;
+
+const getMillisecondsFromSeconds = (seconds) => isFloat(seconds)
+  ? parseInt(`${(seconds)}0000`.replace(/\d*\./, '0').slice(0, 4))
+  : 0;
+
+export const toUnixTimestamp = (isoDatetime) => {
+  if (!isValid(isoDatetime)) { return INVALID_DATETIME; }
+
+  const fragments = toFragments(isoDatetime);
+  const days = daysSinceEpoch(fragments);
+  const milliseconds = getMillisecondsFromSeconds(fragments.second);
+
+  return compose(
+    (sum) => sum + days * 24 * 60 * 60 * 1000,
+    (sum) => sum + (fragments.hour || 0) * 60 * 60 * 1000,
+    (sum) => sum + (fragments.minute || 0) * 60 * 1000,
+    (sum) => sum + floor((fragments.second || 0)) * 1000,
+    (sum) => sum + milliseconds,
+  )(0);
 };
--- src/datetime/transformations/unix-timestamp.spec.js
+++ src/datetime/transformations/unix-timestamp.spec.js
@@ -1,6 +1,6 @@
 import ci from 'ci-info';
 import { assertThat, equalTo } from 'hamjest';
-import { fromUnixTimestamp } from '../index';
+import { fromUnixTimestamp, toUnixTimestamp, daysSinceEpoch } from './unix-timestamp';
 
 describe('fromUnixTimestamp', () => {
   it('unix timestamp 0 responds 1970-01-01T00:00:00', () => assertThat(
@@ -9,25 +9,6 @@ describe('fromUnixTimestamp', () => {
   it('unix timestamp 1 responds 1700-01-01T00:00:01', () => assertThat(
     fromUnixTimestamp(1), equalTo('1970-01-01T00:00:01')));
 });
-
-const floor = (value) => Math.floor(value);
-const daysSinceEpoch = ({ year: _year, month: m, day: d }) => {
-  const year = m <= 2
-    ? _year - 1
-    : _year
-  ;
-
-  const era = floor((year >= 0
-    ? year
-    : year - 399
-  ) / 400);
-
-  const yearOfEra = year - era * 400;
-  const dayOfYear = floor((153 * (m + (m > 2 ? -3 : 9)) + 2) / 5) + d - 1;
-  const dayOfEra = yearOfEra * 365 + floor(yearOfEra / 4) - floor(yearOfEra / 100) + dayOfYear;
-
-  return era * 146097 + dayOfEra - 719468;
-};
 
 describe('daysSinceEpoch', () => {
   it('1969-12-30, responds -2', () => assertThat(
@@ -45,30 +26,6 @@ describe('daysSinceEpoch', () => {
   it('1970-01-03, responds 2', () => assertThat(
     daysSinceEpoch({ year: 1970, month: 1, day: 3 }), equalTo(2)));
 });
-
-
-import { toFragments } from '../index';
-import { compose } from '../../utils';
-
-const isFloat = (value) => !isNaN(value) && value.toString().indexOf('.') != -1;
-
-const getMillisecondsFromSeconds = (seconds) => isFloat(seconds)
-  ? parseInt(`${(seconds)}0000`.replace(/\d*\./, '0').slice(0, 4))
-  : 0;
-
-const toUnixTimestamp = (isoDatetime) => {
-  const fragments = toFragments(isoDatetime);
-  const days = daysSinceEpoch(fragments);
-  const milliseconds = getMillisecondsFromSeconds(fragments.second);
-
-  return compose(
-    (sum) => sum + days * 24 * 60 * 60 * 1000,
-    (sum) => sum + (fragments.hour || 0) * 60 * 60 * 1000,
-    (sum) => sum + (fragments.minute || 0) * 60 * 1000,
-    (sum) => sum + floor((fragments.second || 0)) * 1000,
-    (sum) => sum + milliseconds,
-  )(0);
-};
 
 describe('toUnixTimestamp', () => {
   if (ci.isCI) { // only run those tests on CI
