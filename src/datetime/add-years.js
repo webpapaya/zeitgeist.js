d8b7cdcf11b1796ad5b19c4899ce3feb6e56dfd1
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-06-05 15:31:48 +0200

    Try to fix travis.

--- src/datetime/add-years.js
+++ src/datetime/add-years.js
@@ -2,9 +2,9 @@ import { curry } from '../utils';
 import { toFragments, toIso } from './index';
 import calculationDecorator from './_internal/calculation-decorator';
 
-const addYears = calculationDecorator(curry((years, isoDatetime) => {
+const addYears = (...args) => calculationDecorator(curry((years, isoDatetime) => {
   const fragments = toFragments(isoDatetime);
   return toIso({ ...fragments, year: fragments.year + years });
-}));
+}))(...args); // TODO: FIXME! travis CI issue
 
 export default addYears;
