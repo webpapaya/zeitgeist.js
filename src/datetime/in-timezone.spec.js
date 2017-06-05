04c99378263c4bdd7e8e7e97b1a187bdc865e30f
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-06-05 12:01:30 +0200

    Refactor and restructure.

--- src/datetime/in-timezone.spec.js
+++ src/datetime/in-timezone.spec.js
@@ -54,19 +54,23 @@ const inTimezone = (timezoneName, isoDatetime) => {
 
 describe('inTimezone', () => {
   it('2000-01-01T00:00:00+00:00 in Europe/Vienna', () => {
-    assertThat(inTimezone('Europe/Vienna', '2000-01-01T00:00:00+00:00'), equalTo('2000-01-01T01:00:00+01:00'))
+    assertThat(inTimezone('Europe/Vienna', '2000-01-01T00:00:00+00:00'),
+      equalTo('2000-01-01T01:00:00+01:00'))
   });
 
   it('2000-01-01T00:00:00+00:00 in Europe/Vienna', () => {
-    assertThat(inTimezone('Europe/Vienna', '2000-06-01T00:00:00+00:00'), equalTo('2000-06-01T02:00:00+02:00'))
+    assertThat(inTimezone('Europe/Vienna', '2000-06-01T00:00:00+00:00'),
+      equalTo('2000-06-01T02:00:00+02:00'))
   });
 
   it('2000-01-01T00:00:00+00:00 in America/New_York', () => {
-    assertThat(inTimezone('America/New_York', '2000-01-01T00:00:00+00:00'), equalTo('1999-12-31T19:00:00-05:00'))
+    assertThat(inTimezone('America/New_York', '2000-01-01T00:00:00+00:00'),
+      equalTo('1999-12-31T19:00:00-05:00'))
   });
 
   it('2000-01-01T01:00:00+01:00 in Europe/Vienna', () => {
-    assertThat(inTimezone('Europe/Vienna', '2000-01-01T01:00:00+01:00'), equalTo('2000-01-01T01:00:00+01:00'))
+    assertThat(inTimezone('Europe/Vienna', '2000-01-01T01:00:00+01:00'),
+      equalTo('2000-01-01T01:00:00+01:00'))
   });
 });
 
--- src/datetime/to-utc.js
+++ src/datetime/to-utc.js
@@ -7,16 +7,20 @@ import setTimezoneOffset from './set-timezone-offset';
 
 const timezoneOffsetToMinutes = (timezoneOffset) => {
   if(!timezoneOffset) { return 0; }
-  const [hours, minutes] = timezoneOffset.split(':').map((unit) => parseInt(unit));
+  const [hours, minutes] = timezoneOffset.split(':')
+    .map((unit) => parseInt(unit));
+
   return (hours * ONE_HOUR + minutes * ONE_MINUTE) / ONE_MINUTE;
 };
 
-const toUtc = (isoDatetime) => {
-  const timezoneOffset = timezoneOffsetToMinutes(getTimezone(isoDatetime));
-  return pipe(
-    subtractMinutes(timezoneOffset),
-    setTimezoneOffset('+00:00'),
-  )(isoDatetime);
-};
+const getTimezoneInMinutes = (isoDatetime) => pipe(
+  getTimezone,
+  timezoneOffsetToMinutes,
+)(isoDatetime);
+
+const toUtc = (isoDatetime) => pipe(
+  subtractMinutes(getTimezoneInMinutes(isoDatetime)),
+  setTimezoneOffset('+00:00'),
+)(isoDatetime);
 
 export default toUtc;
