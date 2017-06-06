17f266a7f1b3a62024f78d06d505824c13a58463
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-06-06 18:53:48 +0200

    Fix broken tests.

--- src/datetime/to-utc.js
+++ src/datetime/to-utc.js
@@ -3,6 +3,7 @@ import { ONE_HOUR, ONE_MINUTE } from './constants';
 
 import subtractMinutes from './subtract-minutes';
 import setTimezoneOffset from './set-timezone-offset';
+import getTimezoneOffset from './get-timezone-offset';
 
 const timezoneOffsetToMinutes = (timezoneOffset) => {
   if (!timezoneOffset) { return 0; }
