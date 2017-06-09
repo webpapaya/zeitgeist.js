7040513a06d18819541efa6e207e2331bd1c6a69
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-06-09 09:30:57 +0200

    Refactored intoToUntil fn.

--- src/datetime/_internal/unpack-tz-file.js
+++ src/datetime/_internal/unpack-tz-file.js
@@ -1,23 +1,21 @@
 // Extracted from TODO: add file
 
-import { execute } from '../../../scripts/utils'
-
-function charCodeToInt(charCode) {
+const charCodeToInt = (charCode) => {
   if (charCode > 96) { return charCode - 87; }
   if (charCode > 64) { return charCode - 29; }
 
   return charCode - 48;
-}
+};
 
 function unpackBase60(string) {
-  let i = 0,
-    parts = string.split('.'),
-    whole = parts[0],
-    fractional = parts[1] || '',
-    multiplier = 1,
-    num,
-    out = 0,
-    sign = 1;
+  let i = 0;
+  let parts = string.split('.');
+  let whole = parts[0];
+  let fractional = parts[1] || '';
+  let multiplier = 1;
+  let num;
+  let out = 0;
+  let sign = 1;
 
   // handle negative numbers
   if (string.charCodeAt(0) === 45) {
@@ -44,15 +42,19 @@ function unpackBase60(string) {
 const arrayToInt = (array) =>
   array.map(unpackBase60);
 
-function intToUntil(array, length) {
-  for (let i = 0; i < length; i++) {
-    array[i] = Math.round((array[i - 1] || 0) + (array[i] * 60000)); // minutes to milliseconds
-  }
+const insertAt = (index, value, array) => {
+  const _array = [...array];
+  _array[index] = value;
+  return _array;
+};
 
-  array[length - 1] = Infinity;
+const intToUntil = (array, index = 0) => {
+  if (index === array.length) { return array; }
 
-  return array;
-}
+  const until = Math.round((array[index - 1] || 0) + (array[index] * 60000));
+
+  return intToUntil(insertAt(index, until, array), index + 1);
+};
 
 const mapIndices = (source, indices) =>
   indices.map((i) => source[indices[i]]);
@@ -61,10 +63,10 @@ const unpack = (data) => {
   const parts = data.split('|');
 
   const indices = arrayToInt(parts[3].split(''));
-  const untils = arrayToInt(parts[4].split(' '));
+  let untils = arrayToInt(parts[4].split(' '));
   const offsets = mapIndices(arrayToInt(parts[2].split(' ')), indices);
 
-  intToUntil(untils, indices.length);
+  untils = intToUntil(untils);
 
   const abbrs = mapIndices(parts[1].split(' '), indices);
 
@@ -72,6 +74,5 @@ const unpack = (data) => {
     return { until: untils[index], abbr: abbrs[index], offset: offsets[index] };
   });
 };
-
 
 export default unpack;
